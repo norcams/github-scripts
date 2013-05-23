04b6250a09fe33f2a84ef4475ca631c2842224a2
elliottcable <me@ell.io>
2013-05-23 19:16:28 -0400

    Enabling *all* notifications.

--- repo_add_irc.sh
+++ repo_add_irc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-owner=mozilla-servo
-repo=$1
-user=metajack
+owner=$1
+repo=$2
+user=CHANGEME
 
 curl -i -u $user -X POST https://api.github.com/repos/$owner/$repo/hooks --data @irc.json
